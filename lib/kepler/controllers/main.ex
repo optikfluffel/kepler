a0ae5415674341868940b79dc83765b53eb9fa17
Udo Kramer <optikfluffel@gmail.com>
2014-04-03 14:59:00 +0200

    add some basic documentation to test my new post-commit hook
    can be found here: https://gist.github.com/optikfluffel/9953679

--- lib/kepler/controllers/main.ex
+++ lib/kepler/controllers/main.ex
@@ -1,6 +1,13 @@
 defmodule Kepler.Controllers.Main do
+  @moduledoc """
+    Handles the main routes.
+  """
+
   use Sugar.Controller
 
+  @doc """
+    Returns `Hello world` with status code `200`.
+  """
   def index(conn, []) do
     raw conn |> resp(200, "Hello world")
   end
